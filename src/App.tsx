5b0dfbab72a805cf286634e85fb8485ce6732b1c
NewtakaNghia <nghia.trantrong0409@hcmut.edu.vn>
2025-09-15 22:08:22 +0700

    Fixing Router in Browser Router

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/code-my-pallette">
+        <BrowserRouter basename="/code-my-palette">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
